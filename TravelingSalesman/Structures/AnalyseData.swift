16c8b2d15dd6cf8309217b91d9e26bd7facc182f
MatrylOne <matryl1@me.com>
2018-03-15 09:52:39 +0100

    Added structure to analyse algorythms

---
+++ TravelingSalesman/Structures/AnalyseData.swift
@@ -0,0 +1,15 @@
+//
+//  AnalyseData.swift
+//  TravelingSalesman
+//
+//  Created by Jakub Nadolny on 15.03.2018.
+//  Copyright © 2018 Jakub Nadolny. All rights reserved.
+//
+
+import Foundation
+
+struct AnalyseData{
+    let worst:Int
+    let average:Int
+    let best:Int
+}
